cfa741f37b3484a704169f925b5b1c85bd18d179
i-zokirov <izokirov@outlook.com>
2023-01-17 19:20:16 +0100

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +11,11 @@ const bot = new Telegraf(token!);
 
 const app = express();
 
-bot.on("text", (ctx) => ctx.reply("Hello"));
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.start((ctx) => ctx.reply("Welcome"));
 
+bot.on(message("text"), (ctx) => ctx.reply("👍"));
+
+bot.launch();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on port", port));
